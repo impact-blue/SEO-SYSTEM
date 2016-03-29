99fcf695554427c496d73ffd4baf9f3aa949cf60
西村健人 <negai.m.nishimura@gmail.com>
2016-03-29 17:11:19 +0900

    [fix] time_outを30分に　googleにsearchを追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -18,59 +18,57 @@ class TopController < ApplicationController
     end
   end
 
-
+#次へのやつがまだできてない。
   def google
-    agent = Mechanize.new
-    agent.user_agent_alias = 'Windows Mozilla'
-    # Mechanizeは便利なUAのエイリアスがあるので、その中から設定が簡単です。
-    ua = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
-    agent.user_agent = ua
-    agent.open_timeout = 180
-    keyword = 'site:http://www.kangoworks.com'
-    escaped_keyword = CGI.escape(keyword)
-    # 検索結果を開く
-    page = agent.get("http://www.google.co.jp/search?ie=UTF-8&oe=UTF-8&q=#{escaped_keyword}")
-    num = 1
-    while true
-        elements = page.search('.g .r a')
-        elements.each do |ele|
-          meta_info = MetaInfo.new
-          meta_info.search_word = keyword
-          href = ele.attribute("href")
-          #link_address
-          meta_info.link_address = URI.parse(href).query.split("&")[0].split("=")[1]
-          #link_text
-          meta_info.link_text = ele.inner_text
-          link = Nokogiri.HTML(open(meta_info.link_address))
-          #title
-          meta_info.title = link.title
-          #description
-          if link.css('//meta[name$=description]/@content').to_s.blank?
-            meta_info.description = link.css('//meta[name$=Description]/@content').to_s
+        agent = Mechanize.new
+        agent.user_agent_alias = 'Windows Mozilla'
+        # Mechanizeは便利なUAのエイリアスがあるので、その中から設定が簡単です。
+        ua = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
+        agent.user_agent = ua
+        agent.open_timeout = 1800
+        keyword = 'site:http://www.kangoworks.com'
+        escaped_keyword = CGI.escape(keyword)
+        # 検索結果を開く
+        page = agent.get("http://www.google.co.jp/search?ie=UTF-8&oe=UTF-8&q=#{escaped_keyword}")
+        num = 1
+        while true
+            elements = page.search('.g .r a')
+            elements.each do |ele|
+              meta_info = MetaInfo.new
+              meta_info.search_word = keyword
+              href = ele.attribute("href")
+              #link_address
+              meta_info.link_address = URI.parse(href).query.split("&")[0].split("=")[1]
+              #link_text
+              meta_info.link_text = ele.inner_text
+              link = Nokogiri.HTML(open(meta_info.link_address))
+              #title
+              meta_info.title = link.title
+              #description
+              if link.css('//meta[name$=description]/@content').to_s.blank?
+                meta_info.description = link.css('//meta[name$=Description]/@content').to_s
+              else
+                meta_info.description = link.css('//meta[name$=description]/@content').to_s
+              end
+              #keywords
+              if link.css('//meta[name$=keywords]/@content').to_s.blank?
+                meta_info.keywords = link.css('//meta[name$=Keywords]/@content').to_s
+              else
+                meta_info.keywords = link.css('//meta[name$=keywords]/@content').to_s
+              end
+              meta_info.save
+            end
+
+          href = page.search("td .b a")[0].attributes["href"]
+          if num == 1
+            page = page.links[60].click
+            num += 1
           else
-            meta_info.description = link.css('//meta[name$=description]/@content').to_s
+            page = page.links[62].click
           end
-          #keywords
-          if link.css('//meta[name$=keywords]/@content').to_s.blank?
-            meta_info.keywords = link.css('//meta[name$=Keywords]/@content').to_s
-          else
-            meta_info.keywords = link.css('//meta[name$=keywords]/@content').to_s
-          end
-          meta_info.save
         end
-
-      href = page.search("td .b a")[0].attributes["href"]
-      if num == 1
-        page = page.links[60].click
-        num += 1
-      else
-        page = page.links[62].click
-      end
-
-      unless href
-        break
-      end
-    end
+      rescue
+        redirect_to root_path
   end
 
 
@@ -81,7 +79,7 @@ class TopController < ApplicationController
         # Mechanizeは便利なUAのエイリアスがあるので、その中から設定が簡単です。
         ua = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
         agent.user_agent = ua
-        agent.open_timeout = 180
+        agent.open_timeout = 1800
         keyword = params[:search] #'site:http://www.kangoworks.com'
         escaped_keyword = CGI.escape(keyword)
         # 検索結果を開く
